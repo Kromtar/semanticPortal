68572800b23aeaed1edaa9bc0dbb9f229b178fd8
Federico Jensen Carrasco <federicodaniel25@gmail.com>
2018-06-11 15:20:44 -0400

    Fix fonts

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import 'materialize-css/dist/js/materialize.min.js'
 import 'materialize-css/dist/css/materialize.min.css';
 import './css/customCss.css';
-import 'typeface-roboto';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
